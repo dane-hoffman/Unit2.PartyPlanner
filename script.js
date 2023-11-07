9b068711bebeb1d1cffcbf8809716103b59e603f
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-07 10:05:11 -0700

    Outlined early Psuedocode, and provided basic html structure.

---
+++ script.js
@@ -0,0 +1,8 @@
+//Fetch is used to GET party data from the API
+//The app contains a descriptive list of all the parties;
+    //names,
+    //dates,
+    //times,
+    //locations,
+//The DOM is dynamically rendered according to data stored in state
+    //The data stored in state is updated to staty in sync with the API
