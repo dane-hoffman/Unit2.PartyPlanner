984e5e209346bde98e914a6cbec84a86f48a498d
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-07 11:02:15 -0700

    Created <button> element, append to <main> upon window(load).

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const header = document.querySelector('header');
+//Grab the <main> element
+const main = document.querySelector('main');
+console.log(main);
 
+//When the window loads
 window.addEventListener('load', () => {
-    const headerElement = document.createElement('h1');
-    headerElement.textContent = 'FSA Party Planner';
-    header.appendChild(headerElement);
     
+    const headerElement = document.createElement('h1');//create <h1> element
+    //create <button> element
+    const mainButton = document.createElement('button');
+    // have <button> intitially say "List of Parities"
+    headerElement.textContent = 'FSA Party Planner';//have <h1> initially say "FSA Party Planner"
+    header.appendChild(headerElement);//append it to the <main>
+    //append the button to the main, below the <h1>
+    main.appendChild(mainButton);
+
+
 });
-console.log(header);
 //Fetch is used to GET party data from the API
 //The app contains a descriptive list of all the parties;
     //names,
